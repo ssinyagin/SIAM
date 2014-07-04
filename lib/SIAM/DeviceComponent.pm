395c730b16bad7da58220d0b4c4f024f043c1cac
Stanislav Sinyagin <ssinyagin@k-open.com>
2014-07-04 16:52:29 +0200

    new attributes: siam.devc.is_attached, siam.devc.attached_to

--- lib/SIAM/DeviceComponent.pm
+++ lib/SIAM/DeviceComponent.pm
@@ -14,7 +14,45 @@ SIAM::DeviceComponent - Device Component object class
 
 =head1 METHODS
 
+=head2 is_attached
+
+Returns true if the device component is attached to another device
+component within the same device.
+
 =cut
+
+sub is_attached
+{
+    my $self = shift;
+    return( $self->attr('siam.devc.is_attached') ? 1:0 );
+}
+
+
+=head2 attached_to
+
+Returns a SIAM::DeviceComponent object to which this object is attached.
+
+=cut
+
+sub attached_to
+{
+    my $self = shift;
+    if( $self->is_attached() )
+    {
+        my $id = $self->attr('siam.devc.attached_to');
+        if( not defined($id) )
+        {
+            $self->error('siam.devc.attached_to is undefined in ' . $self->id);
+            return undef;
+        }
+        return $self->instantiate_object('SIAM::DeviceComponent', $id);
+    }
+
+    return;
+}
+
+
+
 
     
 # mandatory attributes
@@ -26,6 +64,10 @@ my $mandatory_attributes =
       'siam.devc.full_name',
       'siam.devc.description' ];
 
+my $optional_attributes =
+    [ 'siam.devc.is_attached',
+      'siam.devc.attached_to' ];
+
 sub _mandatory_attributes
 {
     return $mandatory_attributes;
@@ -34,7 +76,7 @@ sub _mandatory_attributes
 
 sub _manifest_attributes
 {
-    return $mandatory_attributes;
+    return [@{$mandatory_attributes}, @{$optional_attributes}];
 }
 
 
